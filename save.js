d8774c5c8d439be0592c445a433c61a3018efb89
nikhil6392 <nikhilpathak210431@gmail.com>
2025-06-01 15:45:38 +0530

    pushing addresses

---
+++ save.js
@@ -0,0 +1,15 @@
+export function saveAddress(addresses, address){
+    if(address.id){
+        const index = addresses.findIndex((adr) => {
+            return adr.id === parseInt(address.id, 10)
+        });
+
+        address.id = parseInt(address.id, 10);
+        addresses[index] = address;
+    } else {
+        const nextId = Math.max(...addresses.map((address) => address.id)) + 1;
+        address.id = nextId;
+        addresses.push(address)
+    }
+    return address
+}
